befd11b06368fc346ffd3faa012db61dce1495a6
Alebik0 <belash.aleksandr777@gmail.com>
2026-07-06 21:16:53 +0300

    /users/[id] page

---
+++ frontend/components/errors/500/Error404Card.tsx
@@ -0,0 +1,37 @@
+"use server";
+
+import Link from "next/link";
+import Error500Icon from "./Error500Icon";
+
+async function Error404Card() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="max-w-lg text-center">
+        <Error500Icon />
+
+        <p className="on-error-container text-sm font-semibold uppercase tracking-wider">
+          Error 404
+        </p>
+
+        <h1 className="on-surface mt-2 text-5xl font-bold tracking-tight">
+          Content Not Found
+        </h1>
+
+        <div className="mt-10 flex flex-col justify-center gap-4 sm:flex-row">
+          <Link
+            href="/"
+            className="primary-container on-primary-container rounded-lg px-6 py-3 font-medium transition hover-secondary-container hover-on-secondary-container"
+          >
+            Go Home
+          </Link>
+        </div>
+
+        <p className="on-surface-variant mt-8 text-sm">
+          If the problem persists, please contact support.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Error404Card;
